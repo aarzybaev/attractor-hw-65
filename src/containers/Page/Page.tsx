ad043a1ad32a212248f413b4a181a039c3b1ecb7
Askhad Arzybaev <aarzybaev@gmail.com>
2024-02-16 10:07:25 +0600

    created Page component

---
+++ src/containers/Page/Page.tsx
@@ -0,0 +1,56 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {ApiPage} from '../../type';
+import axiosAPI from '../../axiosAPI';
+import Spinner from '../../components/Spinner/Spinner';
+
+const Page: React.FC = () => {
+  let { pageName} = useParams();
+
+  if (!pageName) {
+    pageName = 'home';
+  }
+  console.log('[PAGE]', pageName);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState<ApiPage | null>(null);
+
+  const fetchPage = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const response = await axiosAPI.get<ApiPage | null>('/pages/' + pageName + '.json');
+      if (response.data) {
+        setPage(response.data);
+      } else {
+        setPage(null);
+      }
+    } catch (e) {
+      console.log(e);
+    }  finally {
+      setIsLoading(false);
+    }
+
+  }, [pageName]);
+
+  useEffect(() => {
+    void fetchPage();
+  }, [fetchPage]);
+
+  let pageArea = <Spinner/>;
+
+  if (!isLoading && page) {
+    pageArea = (
+      <div>
+        <h3>{page.title}</h3>
+        <p>{page.content}</p>
+      </div>
+    );
+  } else if (!isLoading && !page) {
+    pageArea = (
+      <h1>Not found</h1>
+    );
+  }
+
+  return pageArea;
+};
+
+export default Page;
